325869058627ffc6bc222bc0a444eb97ec8a43ee
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-04-07 01:20:47 +0900

    Update NGCF.py

--- RecSys-basic/NGCF/NGCF.py
+++ RecSys-basic/NGCF/NGCF.py
@@ -7,92 +7,78 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from models.BaseModel import BaseModel
 
-
-class NGCF(BaseModel):
-    def __init__(self, model_conf, num_user, num_item, device):
+class NGCF(nn.Module):
+    def __init__(self, data_name, num_user, num_item, emb_dim, device):
         super(NGCF, self).__init__()
-        self.data_name = model_conf.data_name
+
+        self.weight_dict = nn.ParameterDict()
+        self.item_embedding_pred = None
+        self.user_embedding_pred = None
+        self.item_embedding = nn.Embedding(self.num_items, self.emb_dim)
+        self.user_embedding = nn.Embedding(self.num_users, self.emb_dim)
+        self.data_name = data_name
         self.num_users = num_user
         self.num_items = num_item
+        self.emb_dim = emb_dim
+        self.num_layers = 2
+        self.mess_dropout = 0.1
 
-        self.emb_dim = model_conf.emb_dim
-        self.num_layers = model_conf.num_layers
-        self.node_dropout = model_conf.node_dropout
-        self.mess_dropout = model_conf.mess_dropout
-
-        self.split = model_conf.split
-        self.num_folds = model_conf.num_folds
-
-        self.reg = model_conf.reg
-        self.batch_size = model_conf.batch_size
+        self.reg = 0.0001
+        self.batch_size = 4096
 
         self.Graph = None
         self.data_loader = None
 
-        self.path = model_conf.graph_dir
+        self.path = "graph"
         if not os.path.exists(self.path):
             os.mkdir(self.path)
 
         self.device = device
 
-        self.build_graph()
+        self.reset_parameters()
 
-
-    def build_graph(self):
+    def reset_parameters(self):
         # Init the weight of user-item
-        self.user_embedding = nn.Embedding(self.num_users, self.emb_dim)
-        self.item_embedding = nn.Embedding(self.num_items, self.emb_dim)
         nn.init.normal_(self.user_embedding.weight, 0, 0.01)
         nn.init.normal_(self.item_embedding.weight, 0, 0.01)
 
-
-        self.weight_dict = nn.ParameterDict()
         layers = [self.emb_dim] * (self.num_layers + 1)
-        
+
         for k in range(len(layers) - 1):
             # W matrix and bias for graph convolution
             self.weight_dict.update(
                 {'W_gc_%d' % k: nn.Parameter(nn.init.normal_(torch.empty(layers[k], layers[k + 1])))})
             self.weight_dict.update({'b_gc_%d' % k: nn.Parameter(nn.init.normal_(torch.empty(1, layers[k + 1])))})
 
-
             # W matrix and bias for bi messages of neighbors
             self.weight_dict.update(
                 {'W_bi_%d' % k: nn.Parameter(nn.init.normal_(torch.empty(layers[k], layers[k + 1])))})
             self.weight_dict.update({'b_bi_%d' % k: nn.Parameter(nn.init.normal_(torch.empty(1, layers[k + 1])))})
 
-        self.user_embedding_pred = None
-        self.item_embedding_pred = None
-
         self.to(self.device)
 
-
-    def train_one_epoch(self, dataset, optimizer, batch_size, verbose):
+    def train(self, dataset, optimizer, batch_size, verbose):
         train_matrix = dataset.train_matrix
 
-        if self.Graph == None:
-            self.Graph = self.getSparseGraph(train_matrix)
-        if self.data_loader == None:
-            self.data_loader = PairwiseGenerator(train_matrix,
-                                                 num_negatives=1,
-                                                 batch_size=self.batch_size,
-                                                 shuffle=True,
-                                                 device=self.device)
+        self.data_loader = PairwiseGenerator(train_matrix,
+                                             num_negatives=1,
+                                             batch_size=self.batch_size,
+                                             shuffle=True,
+                                             device=self.device)
 
         loss = 0.0
         for idx, batch_data in enumerate(self.data_loader):
             optimizer.zero_grad()
             batch_user, batch_pos, batch_neg = batch_data
 
-            pos_output, neg_output = self.forward(batch_user, batch_pos, batch_neg)
-            userEmb0 = self.user_embedding(batch_user)
-            posEmb0 = self.item_embedding(batch_pos)
-            negEmb0 = self.item_embedding(batch_neg)
+            pos_out, neg_out = self.forward(batch_user, batch_pos, batch_neg)
+            user_emb = self.user_embedding(batch_user)
+            pos_emb = self.item_embedding(batch_pos)
+            neg_emb = self.item_embedding(batch_neg)
 
-            batch_loss = torch.mean(F.softplus(neg_output - pos_output))
-            reg_loss = (1 / 2) * (userEmb0.norm(2).pow(2) + posEmb0.norm(2).pow(2) + negEmb0.norm(2).pow(2)) / float(
+            batch_loss = torch.mean(F.softplus(neg_out - pos_out))
+            reg_loss = (1 / 2) * (user_emb.norm(2).pow(2) + pos_emb.norm(2).pow(2) + neg_emb.norm(2).pow(2)) / float(
                 len(batch_user))
 
             batch_loss = batch_loss + self.reg * reg_loss
@@ -106,136 +92,42 @@ class NGCF(BaseModel):
                 print('(%3d / %3d) loss = %.4f' % (idx, num_batches, batch_loss))
         return loss
 
-
-    def getSparseGraph(self, rating_matrix):
-        n_users, n_items = rating_matrix.shape
-        print("loading adjacency matrix")
-
-        filename = f'{self.data_name}_s_pre_adj_mat.npz'
-        try:
-            pre_adj_mat = sp.load_npz(os.path.join(self.path, filename))
-            print("successfully loaded...")
-            norm_adj = pre_adj_mat
-        except:
-            print("generating adjacency matrix")
-            s = time.time()
-            adj_mat = sp.dok_matrix((n_users + n_items, n_users + n_items), dtype=np.float32)
-            adj_mat = adj_mat.tolil()
-            R = rating_matrix.tolil()
-            adj_mat[:n_users, n_users:] = R    # N * M
-            adj_mat[n_users:, :n_users] = R.T  # M * N
-            adj_mat = adj_mat.todok()
-
-            rowsum = np.array(adj_mat.sum(axis=1))
-            d_inv = np.power(rowsum, -0.5).flatten()
-            d_inv[np.isinf(d_inv)] = 0.
-            d_mat = sp.diags(d_inv)
-            # print("*** d_mat", d_mat)
-
-            norm_adj = d_mat.dot(adj_mat)
-            norm_adj = norm_adj.dot(d_mat)
-            norm_adj = norm_adj.tocsr()
-            end = time.time()
-            print(f"costing {end - s}s, saved norm_mat...")
-            sp.save_npz(os.path.join(self.path, filename), norm_adj)
-
-        if self.split == True:
-            Graph = self._split_A_hat(norm_adj)
-            print("done split matrix")
-        else:
-            Graph = self._convert_sp_mat_to_sp_tensor(norm_adj)
-            Graph = Graph.coalesce().to(self.device)
-            print("don't split the matrix")
-
-        return Graph
-
-    def _split_A_hat(self, A):
-        A_fold = []
-        fold_len = (self.num_users + self.num_items) // self.num_folds
-        for i_fold in range(self.num_folds):
-            start = i_fold * fold_len
-            if i_fold == self.num_folds - 1:
-                end = self.num_users + self.num_items
-            else:
-                end = (i_fold + 1) * fold_len
-            A_fold.append(self._convert_sp_mat_to_sp_tensor(A[start:end]).coalesce().to(self.device))
-        return A_fold
-
-    def _convert_sp_mat_to_sp_tensor(self, X):
-        coo = X.tocoo().astype(np.float32)
-        row = torch.Tensor(coo.row).long()
-        col = torch.Tensor(coo.col).long()
-        index = torch.stack([row, col])
-        data = torch.FloatTensor(coo.data)
-        return torch.sparse.FloatTensor(index, data, torch.Size(coo.shape))
-
     def forward(self, user, pos, neg=None):
-        u_embedding, i_embedding = self.ngcf_embedding(self.Graph)
+        u, i = self.get_emb(self.Graph)
 
-        user_latent = F.embedding(user, u_embedding)
-        pos_latent = F.embedding(pos, i_embedding)
-        
+        user_latent = F.embedding(user, u)
+        pos_latent = F.embedding(pos, i)
+
         pos_score = torch.mul(user_latent, pos_latent).sum(1)
         if neg is not None:
             # BPR loss
-            neg_latent = F.embedding(neg, i_embedding)
+            neg_latent = F.embedding(neg, i)
             neg_score = torch.mul(user_latent, neg_latent).sum(1)
             return pos_score, neg_score
         else:
             return pos_score
 
-    def __dropout_x(self, x, keep_prob):
-        size = x.size()
-        index = x.indices().t()
-        values = x.values()
-        random_index = torch.rand(len(values)) + keep_prob
-        random_index = random_index.int().bool()
-        index = index[random_index]
-        values = values[random_index] / keep_prob
-        g = torch.sparse.FloatTensor(index.t(), values, size)
-        return g
-
-    def __dropout(self, keep_prob):
-        if self.split:
-            graph = []
-            for g in self.Graph:
-                graph.append(self.__dropout_x(g, keep_prob))
-        else:
-            graph = self.__dropout_x(self.Graph, keep_prob)
-        return graph
-
-    def ngcf_embedding(self, graph):
-        users_emb = self.user_embedding.weight
-        items_emb = self.item_embedding.weight
-        all_emb = torch.cat([users_emb, items_emb])
+    def get_emb(self, graph):
+        user_emb = self.user_embedding.weight
+        item_emb = self.item_embedding.weight
+        all_emb = torch.cat([user_emb, item_emb])
         print(all_emb.shape)
 
         embs = [all_emb]
-        if self.node_dropout > 0:
-            if self.training:
-                g_droped = self.__dropout(graph, self.node_dropout)
-            else:
-                g_droped = graph
-        else:
-            g_droped = graph
-
         ego_emb = all_emb  # (N + M) x d
+
         for k in range(self.num_layers):
-            if self.split:
-                temp_emb = []
-                for f in range(len(g_droped)):
-                    temp_emb.append(torch.sparse.mm(g_droped[f], ego_emb))
-                side_emb = torch.cat(temp_emb, dim=0)
-            else:
-                side_emb = torch.sparse.mm(g_droped, ego_emb)  # ((N + M) x (N + M)) * ((N + M) x d) = (N + M) x d
+            side_emb = torch.sparse.mm(graph, ego_emb)  # ((N + M) x (N + M)) * ((N + M) x d) = (N + M) x d
 
             # transformed sum messages of neighbors
-            sum_emb = torch.matmul(side_emb, self.weight_dict['W_gc_%d' % k]) + self.weight_dict['b_gc_%d' % k]  # (N + M) x d'
+            sum_emb = torch.matmul(side_emb, self.weight_dict['W_gc_%d' % k]) + self.weight_dict[
+                'b_gc_%d' % k]  # (N + M) x d'
 
             # element-wise product
             bi_emb = torch.mul(ego_emb, side_emb)  # (N + M) x d
             # transformed bi messages of neighbors
-            bi_emb = torch.matmul(bi_emb, self.weight_dict['W_bi_%d' % k]) + self.weight_dict['b_bi_%d' % k]     # (N + M) x d'
+            bi_emb = torch.matmul(bi_emb, self.weight_dict['W_bi_%d' % k]) + self.weight_dict[
+                'b_bi_%d' % k]  # (N + M) x d'
 
             # non-linear activation
             ego_emb = F.leaky_relu(sum_emb + bi_emb, negative_slope=0.2)
@@ -252,30 +144,7 @@ class NGCF(BaseModel):
         users, items = torch.split(ngcf_out, [self.num_users, self.num_items])
         return users, items
 
-    def predict_batch_users(self, user_ids):
-        user_embeddings = F.embedding(user_ids, self.user_embedding_pred)
-        item_embeddings = self.item_embedding_pred
-        return user_embeddings @ item_embeddings.T
 
-    def predict(self, eval_users, eval_pos, test_batch_size):
-        num_eval_users = len(eval_users)
-        num_batches = int(np.ceil(num_eval_users / test_batch_size))
-        pred_matrix = np.zeros(eval_pos.shape)
-        perm = list(range(num_eval_users))
-        with torch.no_grad():
-            for b in range(num_batches):
-                if (b + 1) * test_batch_size >= num_eval_users:
-                    batch_idx = perm[b * test_batch_size:]
-                else:
-                    batch_idx = perm[b * test_batch_size: (b + 1) * test_batch_size]
-
-                batch_users = eval_users[batch_idx]
-                batch_users_torch = torch.LongTensor(batch_users).to(self.device)
-                pred_matrix[batch_users] = self.predict_batch_users(batch_users_torch).detach().cpu().numpy()
-
-        pred_matrix[eval_pos.nonzero()] = float('-inf')
-
-        return pred_matrix
-
-    def before_evaluate(self):
-        self.user_embedding_pred, self.item_embedding_pred = self.ngcf_embedding(self.Graph)
+class PairwiseGenerator:
+    def __init__(self, matrix, num_negatives=1, batch_size=32, shuffle=True, device=None):
+        super().__init__()
